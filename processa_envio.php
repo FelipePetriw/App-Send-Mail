3ca63da4a71c9c3f9a8cfe8710885eacb262275c
Felipe Petriw <felipe.petriw.ads@gmail.com>
2023-05-24 15:59:55 -0300

    metodos get e set
    Aplicação dos metodos get e set que trataram das funções da pagina processa_envio.

--- processa_envio.php
+++ processa_envio.php
@@ -4,6 +4,18 @@ class Mensagem {
     private $para = null;
     private $assunto = null;
     private $mensagem = null;
+
+    public function __get($atributo) {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor) {
+        $this->$atributo = $valor;
+    }
+
+    public function mensagemValida() {
+        
+    }
 }
 
 ?>
